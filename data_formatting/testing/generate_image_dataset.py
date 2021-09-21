e655306a86ec0186a8f286c55862969ba484f2b2
WillMandil001 <willmandil@yahoo.co.uk>
2021-09-21 16:42:05 +0100

    working model generator (for images too) and  fixes added to image data generator

--- data_formatting/testing/generate_image_dataset.py
+++ data_formatting/testing/generate_image_dataset.py
@@ -15,16 +15,17 @@ import logging
 import numpy as np
 import pandas as pd
 
-from PIL import Image 
+from PIL import Image
 from tqdm import tqdm
+from pickle import dump
 from dotenv import find_dotenv, load_dotenv
+from sklearn import preprocessing
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 from scipy.spatial.transform import Rotation as R
 from scipy.ndimage.interpolation import map_coordinates
 
 # Hyperparameters:
-
 data_dir = '/home/user/Robotics/Data_sets/slip_detection/will_dataset/will_data_collection/data_collection_001/'
 out_dir = '/home/user/Robotics/Data_sets/slip_detection/test_dataset/'
 context_length = 10
@@ -44,101 +45,207 @@ def create_image(tactile_x, tactile_y, tactile_z):
 	reshaped_image = np.rot90(cv2.resize(image.astype(np.float32), dsize=(image_height, image_width), interpolation=cv2.INTER_CUBIC), k=1, axes=(0, 1))
 	return reshaped_image
 
-# read all the folder names for each trial
-files = glob.glob(data_dir + '/*')
-path_file = []
-index_to_save = 0
+def calculate_scale_parameters():
+	tactile_data_final = []
+	robot_task_space_final = []
+	for experiment_number in tqdm (range (len (files))):
+		robot_task_space = []
+		tactile_data = []
+		robot_state = np.asarray (pd.read_csv (files[experiment_number] + '/robot_state.csv', header=None))
+		xela_sensor1 = np.asarray (pd.read_csv (files[experiment_number] + '/xela_sensor1.csv', header=None))
 
-for experiment_number in tqdm(range(len(files))):
-	robot_state  = np.asarray(pd.read_csv(files[experiment_number] + '/robot_state.csv', header=None))
-	xela_sensor1 = np.asarray(pd.read_csv(files[experiment_number] + '/xela_sensor1.csv', header=None))
-	meta_data = np.asarray(pd.read_csv(files[experiment_number] + '/meta_data.csv', header=None))
+		####################################### Robot Data ###########################################
+		for state in robot_state[1:]:
+			ee_orientation = R.from_quat ([state[-4], state[-3], state[-2], state[-1]]).as_euler ('zyx', degrees=True)
+			robot_task_space.append (
+				[state[-7], state[-6], state[-5], ee_orientation[0], ee_orientation[1], ee_orientation[2]])
+		robot_task_space = np.asarray(robot_task_space).astype(float)
 
-	####################################### Robot Data ###########################################
-	robot_task_space = []
-	for state in robot_state[1:]:
-		ee_orientation = R.from_quat([state[-4], state[-3], state[-2], state[-1]]).as_euler('zyx', degrees=True)
-		robot_task_space.append([state[-7], state[-6], state[-5], ee_orientation[0], ee_orientation[1], ee_orientation[2]])
-	robot_task_space = np.asarray(robot_task_space).astype(float)
-
-	# normalise for each value:
-	min_max = []
-	for feature in range(6):
-		min_max.append([ min(np.array(robot_task_space[:,feature])), max(np.array(robot_task_space)[:,feature]) ])
-
-	for time_step in range(robot_task_space.shape[0]):
+		# normalise for each value:
+		min_max = []
 		for feature in range(6):
-			robot_task_space[time_step][feature] = (robot_task_space[time_step][feature] - min_max[feature][0]) / (min_max[feature][1] - min_max[feature][0])
+			min_max.append([ min(np.array(robot_task_space[:,feature])), max(np.array(robot_task_space)[:,feature]) ])
+		for time_step in range(robot_task_space.shape[0]):
+			for feature in range(6):
+				robot_task_space[time_step][feature] = (robot_task_space[time_step][feature] - min_max[feature][0]) / (min_max[feature][1] - min_max[feature][0])
 
-	####################################### Xela Sensor Data ###########################################
-	tactile_data = []
-	for sample in xela_sensor1[1:]:
-		tactile_data_sample_x, tactile_data_sample_y, tactile_data_sample_z = [], [], []
-		for i in range(0, len(xela_sensor1[0]), 3):
-			tactile_data_sample_x.append(float(sample[i]))
-			tactile_data_sample_y.append(float(sample[i+1]))
-			tactile_data_sample_z.append(float(sample[i+2]))
-		tactile_data.append([tactile_data_sample_x, tactile_data_sample_y, tactile_data_sample_z])
+		####################################### Xela Sensor Data ###########################################
+		for sample in xela_sensor1[1:]:
+			tactile_data_sample_x, tactile_data_sample_y, tactile_data_sample_z = [], [], []
+			for i in range(0, len(xela_sensor1[0]), 3):
+				tactile_data_sample_x.append(float(sample[i]))
+				tactile_data_sample_y.append(float(sample[i+1]))
+				tactile_data_sample_z.append(float(sample[i+2]))
+			tactile_data.append([tactile_data_sample_x, tactile_data_sample_y, tactile_data_sample_z])
 
-	# mean starting values:
-	tactile_mean_start_values = []
-	tactile_offsets = []
-	for feature in range(3):
-		tactile_mean_start_values.append(int(sum(tactile_data[0][feature]) / len(tactile_data[0][feature])))
-		tactile_offsets.append([tactile_mean_start_values[feature] - tactile_starting_value for tactile_starting_value in tactile_data[0][feature]])
-
-	# normalise for each force:
-	tactile_data = np.array(tactile_data)
-	min_max_tactile = []
-	for feature in range(3):
-		min_max_tactile.append([ min([min(x) for x in tactile_data[:,feature]]), max([max(x) for x in tactile_data[:,feature]]) ])
-
-	for time_step in range(tactile_data.shape[0]):
+		# mean starting values:
+		tactile_mean_start_values = []
+		tactile_offsets = []
 		for feature in range(3):
-			tactile_sample_test = [offset + real_value for offset, real_value in zip(tactile_offsets[feature], tactile_data[time_step][feature])]
-			for i in range (tactile_data.shape[2]):
-				tactile_data[time_step][feature][i] = (tactile_sample_test[i] - min_max_tactile[feature][0]) / (min_max_tactile[feature][1] - min_max_tactile[feature][0])
+			tactile_mean_start_values.append(int(sum(tactile_data[0][feature]) / len(tactile_data[0][feature])))
+			tactile_offsets.append([tactile_mean_start_values[feature] - tactile_starting_value for tactile_starting_value in tactile_data[0][feature]])
 
-	# Create & save the images:
-	tactile_images, tactile_image_names = [], []
-	for time_step in range(tactile_data.shape[0]):
-		# create the image:
-		current_image = create_image(tactile_data[time_step][0], tactile_data[time_step][1], tactile_data[time_step][1])
-		tactile_images.append(current_image)
-		# save the image:
-		image_name = "tactile_image_" + str(experiment_number) + "_time_step_" + str(time_step) + ".npy"
-		tactile_image_names.append(image_name)
-		np.save(out_dir + image_name, current_image)
+		# normalise for each force:
+		tactile_data = np.array(tactile_data)
+		min_max_tactile = []
+		for feature in range(3):
+			min_max_tactile.append([ min([min(x) for x in tactile_data[:,feature]]), max([max(x) for x in tactile_data[:,feature]]) ])
+		for time_step in range(tactile_data.shape[0]):
+			for feature in range(3):
+				tactile_sample_test = [offset + real_value for offset, real_value in zip(tactile_offsets[feature], tactile_data[time_step][feature])]
+				for i in range (tactile_data.shape[2]):
+					tactile_data[time_step][feature][i] = (tactile_sample_test[i] - min_max_tactile[feature][0]) / (min_max_tactile[feature][1] - min_max_tactile[feature][0])
 
-	####################################### Format data into time series ###########################################
-	sequence_length = context_length + horrizon_length
-	for sample in range(0, tactile_data.shape[0] - sequence_length):
-		robot_data_euler_sequence, tactile_data_sequence, experiment_data_sequence, time_step_data_sequence, tactile_image_name_sequence = [], [], [], [], []
-		for t in range(0, sequence_length):
-			robot_data_euler_sequence.append(list(robot_task_space[sample+t]))
-			tactile_data_sequence.append(list(tactile_data[sample+t]))
-			tactile_image_name_sequence.append(tactile_image_names[sample + t])
-			experiment_data_sequence.append(experiment_number)
-			time_step_data_sequence.append(sample+t)
+		tactile_data_final += list(tactile_data)
+		robot_task_space_final += list(robot_task_space)
 
-		####################################### Save the data and add to the map ###########################################
-		np.save(out_dir + 'robot_data_euler_' + str(index_to_save), robot_data_euler_sequence)
-		np.save(out_dir + 'xela_1_data_' + str(index_to_save), tactile_data_sequence)
-		np.save(out_dir + 'xela_1_image_data_' + str(index_to_save), tactile_image_name_sequence)
-		np.save(out_dir + 'experiment_number_' + str(index_to_save), experiment_data_sequence)
-		np.save(out_dir + 'time_step_data_' + str(index_to_save), time_step_data_sequence)
-		ref = []
-		ref.append('robot_data_euler_' + str(index_to_save) + '.npy')
-		ref.append('tactile_data_sequence' + str(index_to_save) + '.npy')
-		ref.append('tactile_image_name_sequence' + str(index_to_save) + '.npy')
-		ref.append('experiment_number_' + str(index_to_save) + '.npy')
-		ref.append('time_step_data_' + str(index_to_save) + '.npy')
-		path_file.append(ref)
-		index_to_save += 1
-	break
+		if experiment_number == 1:
+			break
 
-with open(out_dir + '/map.csv', 'w') as csvfile:
-	writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-	writer.writerow(['robot_data_path_euler', 'tactile_data_sequence', 'tactile_image_name_sequence', 'experiment_number', 'time_steps'])
-	for row in path_file:
-		writer.writerow(row)
+	####################################### Find the scalar values ###########################################
+	tactile_data_final = np.array(tactile_data_final)
+	robot_task_space_final = np.array(robot_task_space_final)
+
+	# tactile scalars: standard then min max:
+	tactile_standard_scaler = [preprocessing.StandardScaler().fit (tactile_data_final[:,0]),
+							   preprocessing.StandardScaler().fit (tactile_data_final[:,1]),
+							   preprocessing.StandardScaler().fit (tactile_data_final[:,2])]
+	tactile_min_max_scalar = [preprocessing.MinMaxScaler (feature_range=(0, 1)).fit (tactile_standard_scaler[0].transform(tactile_data_final[:,0])),
+							  preprocessing.MinMaxScaler (feature_range=(0, 1)).fit (tactile_standard_scaler[1].transform(tactile_data_final[:,1])),
+							  preprocessing.MinMaxScaler (feature_range=(0, 1)).fit (tactile_standard_scaler[2].transform(tactile_data_final[:,2]))]
+
+	# robot task space scalars: standard then min max:
+	robot_min_max_scalar = []
+	for feature in range(robot_task_space_final.shape[1]):
+		robot_min_max_scalar.append(preprocessing.MinMaxScaler (feature_range=(0, 1)).fit (robot_task_space_final[:,feature].reshape (-1, 1)))
+
+
+	return tactile_standard_scaler, tactile_min_max_scalar, robot_min_max_scalar
+
+if __name__  == "__main__":
+
+	# read all the folder names for each trial
+	files = glob.glob(data_dir + '/*')
+	path_file = []
+	index_to_save = 0
+
+	# calculate the scalar parameters:
+	tactile_standard_scaler, tactile_min_max_scalar, robot_min_max_scalar = calculate_scale_parameters()
+
+	# save the scalars
+	dump (tactile_standard_scaler[0], open (out_dir + 'tactile_standard_scaler_x.pkl', 'wb'))
+	dump (tactile_standard_scaler[1], open (out_dir + 'tactile_standard_scaler_y.pkl', 'wb'))
+	dump (tactile_standard_scaler[2], open (out_dir + 'tactile_standard_scaler_z.pkl', 'wb'))
+	dump (tactile_min_max_scalar[0], open (out_dir + 'tactile_min_max_scalar_x.pkl', 'wb'))
+	dump (tactile_min_max_scalar[1], open (out_dir + 'tactile_min_max_scalar_y.pkl', 'wb'))
+	dump (tactile_min_max_scalar[2], open (out_dir + 'tactile_min_max_scalar.pkl', 'wb'))
+
+	dump (robot_min_max_scalar[0], open (out_dir + 'robot_min_max_scalar_px.pkl', 'wb'))
+	dump (robot_min_max_scalar[1], open (out_dir + 'robot_min_max_scalar_py.pkl', 'wb'))
+	dump (robot_min_max_scalar[2], open (out_dir + 'robot_min_max_scalar_pz.pkl', 'wb'))
+	dump (robot_min_max_scalar[3], open (out_dir + 'robot_min_max_scalar_ex.pkl', 'wb'))
+	dump (robot_min_max_scalar[4], open (out_dir + 'robot_min_max_scalar_ey.pkl', 'wb'))
+	dump (robot_min_max_scalar[5], open (out_dir + 'robot_min_max_scalar_ez.pkl', 'wb'))
+
+	for experiment_number in tqdm(range(len(files))):
+		robot_state  = np.asarray(pd.read_csv(files[experiment_number] + '/robot_state.csv', header=None))
+		xela_sensor1 = np.asarray(pd.read_csv(files[experiment_number] + '/xela_sensor1.csv', header=None))
+		meta_data = np.asarray(pd.read_csv(files[experiment_number] + '/meta_data.csv', header=None))
+
+		####################################### Robot Data ###########################################
+		robot_task_space = []
+		for state in robot_state[1:]:
+			ee_orientation = R.from_quat([state[-4], state[-3], state[-2], state[-1]]).as_euler('zyx', degrees=True)
+			robot_task_space.append([state[-7], state[-6], state[-5], ee_orientation[0], ee_orientation[1], ee_orientation[2]])
+		robot_task_space = np.asarray(robot_task_space).astype(float)
+
+		# normalise for each value:
+		min_max = []
+		for feature in range(6):
+			min_max.append([ min(np.array(robot_task_space[:,feature])), max(np.array(robot_task_space)[:,feature]) ])
+
+		for time_step in range(robot_task_space.shape[0]):
+			for feature in range(6):
+				robot_task_space[time_step][feature] = (robot_task_space[time_step][feature] - min_max[feature][0]) / (min_max[feature][1] - min_max[feature][0])
+
+		####################################### Xela Sensor Data ###########################################
+		tactile_data = []
+		for sample in xela_sensor1[1:]:
+			tactile_data_sample_x, tactile_data_sample_y, tactile_data_sample_z = [], [], []
+			for i in range(0, len(xela_sensor1[0]), 3):
+				tactile_data_sample_x.append(float(sample[i]))
+				tactile_data_sample_y.append(float(sample[i+1]))
+				tactile_data_sample_z.append(float(sample[i+2]))
+			tactile_data.append([tactile_data_sample_x, tactile_data_sample_y, tactile_data_sample_z])
+
+		# mean starting values:
+		tactile_mean_start_values = []
+		tactile_offsets = []
+		for feature in range(3):
+			tactile_mean_start_values.append(int(sum(tactile_data[0][feature]) / len(tactile_data[0][feature])))
+			tactile_offsets.append([tactile_mean_start_values[feature] - tactile_starting_value for tactile_starting_value in tactile_data[0][feature]])
+
+		# normalise for each force:
+		tactile_data = np.array(tactile_data)
+		min_max_tactile = []
+		for feature in range(3):
+			min_max_tactile.append([ min([min(x) for x in tactile_data[:,feature]]), max([max(x) for x in tactile_data[:,feature]]) ])
+
+		for time_step in range(tactile_data.shape[0]):
+			for feature in range(3):
+				tactile_sample_test = [offset + real_value for offset, real_value in zip(tactile_offsets[feature], tactile_data[time_step][feature])]
+				for i in range (tactile_data.shape[2]):
+					tactile_data[time_step][feature][i] = (tactile_sample_test[i] - min_max_tactile[feature][0]) / (min_max_tactile[feature][1] - min_max_tactile[feature][0])
+
+		####################################### Scale the data ###########################################
+		for index, (standard_scaler, min_max_scalar) in enumerate(zip(tactile_standard_scaler, tactile_min_max_scalar)):
+			tactile_data[:, index] = standard_scaler.transform(tactile_data[:, index])
+			tactile_data[:, index] = min_max_scalar.transform(tactile_data[:, index])
+
+		for index, min_max_scalar in enumerate(robot_min_max_scalar):
+			robot_task_space[:, index] = np.squeeze(min_max_scalar.transform(robot_task_space[:, index].reshape(-1, 1)))
+
+		# Create & save the images:
+		tactile_images, tactile_image_names = [], []
+		for time_step in range(tactile_data.shape[0]):
+			# create the image:
+			current_image = create_image(tactile_data[time_step][0], tactile_data[time_step][1], tactile_data[time_step][1])
+			tactile_images.append(current_image)
+			# save the image:
+			image_name = "tactile_image_" + str(experiment_number) + "_time_step_" + str(time_step) + ".npy"
+			tactile_image_names.append(image_name)
+			np.save(out_dir + image_name, current_image)
+
+		####################################### Format data into time series ###########################################
+		sequence_length = context_length + horrizon_length
+		for sample in range(0, tactile_data.shape[0] - sequence_length):
+			robot_data_euler_sequence, tactile_data_sequence, experiment_data_sequence, time_step_data_sequence, tactile_image_name_sequence = [], [], [], [], []
+			for t in range(0, sequence_length):
+				robot_data_euler_sequence.append(list(robot_task_space[sample+t]))
+				tactile_data_sequence.append(list(tactile_data[sample+t]))
+				tactile_image_name_sequence.append(tactile_image_names[sample + t])
+				experiment_data_sequence.append(experiment_number)
+				time_step_data_sequence.append(sample+t)
+
+			####################################### Save the data and add to the map ###########################################
+			np.save(out_dir + 'robot_data_euler_' + str(index_to_save), robot_data_euler_sequence)
+			np.save(out_dir + 'tactile_data_sequence_' + str(index_to_save), tactile_data_sequence)
+			np.save(out_dir + 'tactile_image_name_sequence_' + str(index_to_save), tactile_image_name_sequence)
+			np.save(out_dir + 'experiment_number_' + str(index_to_save), experiment_data_sequence)
+			np.save(out_dir + 'time_step_data_' + str(index_to_save), time_step_data_sequence)
+			ref = []
+			ref.append('robot_data_euler_' + str(index_to_save) + '.npy')
+			ref.append('tactile_data_sequence_' + str(index_to_save) + '.npy')
+			ref.append('tactile_image_name_sequence_' + str(index_to_save) + '.npy')
+			ref.append('experiment_number_' + str(index_to_save) + '.npy')
+			ref.append('time_step_data_' + str(index_to_save) + '.npy')
+			path_file.append(ref)
+			index_to_save += 1
+
+		break
+
+	with open(out_dir + '/map.csv', 'w') as csvfile:
+		writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+		writer.writerow(['robot_data_path_euler', 'tactile_data_sequence', 'tactile_image_name_sequence', 'experiment_number', 'time_steps'])
+		for row in path_file:
+			writer.writerow(row)
---
+++ models/ACTP/ACTP_model.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+### RUN IN PYTHON 3
+import os
+import cv2
+import csv
+import glob
+import copy
+import click
+import logging
+import numpy as np
+import pandas as pd
+
+from PIL import Image
+from tqdm import tqdm
+from dotenv import find_dotenv, load_dotenv
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+
+from string import digits
+from torch.utils.data import Dataset
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torch.nn.functional as F
+
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+from scipy.spatial.transform import Rotation as R
+from scipy.ndimage.interpolation import map_coordinates
+
+data_dir = '/home/user/Robotics/Data_sets/slip_detection/test_dataset/'
+model_path = "/home/user/Robotics/tactile_prediction/tactile_prediction/models/ACTP/"
+
+seed = 42
+epochs = 100
+batch_size = 32
+learning_rate = 1e-3
+context_frames = 10
+sequence_length = 20
+
+torch.manual_seed(seed)
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")#  use gpu if available
+
+
+class BatchGenerator:
+    def __init__(self):
+        data_map = []
+        with open(data_dir + 'map.csv', 'r') as f:  # rb
+            reader = csv.reader(f)
+            for row in reader:
+                data_map.append(row)
+
+        if len(data_map) <= 1:  # empty or only header
+            print("No file map found")
+            exit()
+
+        self.data_map = data_map
+
+    def load_full_data(self):
+        dataset_train = FullDataSet(self.data_map)
+        transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+        train_loader = torch.utils.data.DataLoader(dataset_train, batch_size=batch_size, shuffle=False)
+        return train_loader
+
+
+class FullDataSet:
+    def __init__(self, data_map):
+        self.samples = data_map[1:]
+        data_map = None
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        value = self.samples[idx]
+        robot_data = np.load(data_dir + value[0])
+        tactile_data = np.load(data_dir + value[1])
+
+        tactile_images = []
+        for image_name in np.load(data_dir + value[2]):
+            tactile_images.append(np.load(data_dir + image_name))
+
+        experiment_number = np.load(data_dir + value[3])
+        time_steps = np.load(data_dir + value[4])
+        return [robot_data.astype(np.float32), tactile_data.astype(np.float32), np.array(tactile_images).astype(np.float32), experiment_number, time_steps]
+
+
+class ATPM(nn.Module):
+    def __init__(self):
+        super(ATPM, self).__init__()
+        self.lstm1 = nn.LSTM(48 + 48, 200).to(device)  # tactile
+        self.lstm2 = nn.LSTM(200, 200).to(device)  # tactile
+        self.fc1 = nn.Linear(200 + 48, 200).to(device)  # tactile + pos_vel
+        self.fc2 = nn.Linear(200, 48).to(device)  # tactile + pos_vel
+        self.tan_activation = nn.Tanh().to(device)
+
+    def forward(self, tactiles, actions):
+        state = actions[0]
+        state.to(device)
+        batch_size__ = tactiles.shape[1]
+        outputs = []
+        hidden1 = (torch.zeros(1,batch_size__,200, device=torch.device('cuda')), torch.zeros(1,batch_size__,200, device=torch.device('cuda')))
+        hidden2 = (torch.zeros(1,batch_size__,200, device=torch.device('cuda')), torch.zeros(1,batch_size__,200, device=torch.device('cuda')))
+
+        for index, (sample_tactile, sample_action,) in enumerate(zip(tactiles.squeeze()[:-1], actions.squeeze()[1:])):
+            # 2. Run through lstm:
+            if index > context_frames-1:
+                out4 = out4 .squeeze()
+                tiled_action_and_state = torch.cat((actions.squeeze()[index+1], state, actions.squeeze()[index+1], state, actions.squeeze()[index+1], state, actions.squeeze()[index+1], state), 1)
+                action_and_tactile = torch.cat((out4, tiled_action_and_state), 1)
+                out1, hidden1 = self.lstm1(action_and_tactile.unsqueeze(0), hidden1)
+                out2, hidden2 = self.lstm2(out1, hidden2)
+                lstm_and_prev_tactile = torch.cat((out2.squeeze(), out4), 1)
+                out3 = self.tan_activation(self.fc1(lstm_and_prev_tactile))
+                out4 = self.tan_activation(self.fc2(out3))
+                outputs.append(out4.squeeze())
+            else:
+                tiled_action_and_state = torch.cat((actions.squeeze()[index+1], state, actions.squeeze()[index+1], state, actions.squeeze()[index+1], state, actions.squeeze()[index+1], state), 1)
+                action_and_tactile = torch.cat((sample_tactile, tiled_action_and_state), 1)
+                out1, hidden1 = self.lstm1(action_and_tactile.unsqueeze(0), hidden1)
+                out2, hidden2 = self.lstm2(out1, hidden2)
+                lstm_and_prev_tactile = torch.cat((out2.squeeze(), sample_tactile), 1)
+                out3 = self.tan_activation(self.fc1(lstm_and_prev_tactile))
+                out4 = self.tan_activation(self.fc2(out3))
+                last_output = out4
+
+        outputs = [last_output] + outputs
+        return torch.stack(outputs)
+
+
+class ModelTrainer:
+    def __init__(self):
+        self.train_full_loader, self.valid_full_loader, self.test_full_loader = BG.load_full_data()
+#         self.full_model = FullModel()
+        self.full_model = torch.load(model_path + "model_t1_corrected")
+        self.criterion = nn.L1Loss()
+        self.criterion1 = nn.L1Loss()
+        self.optimizer = optim.Adam(self.full_model.parameters(), lr=learning_rate)
+
+    def train_full_model(self):
+        plot_training_loss = []
+        plot_validation_loss = []
+        previous_val_mean_loss = 100.0
+        best_val_loss = 100.0
+        early_stop_clock = 0
+        progress_bar = tqdm.tqdm(range(0, epochs), total=(epochs*len(self.train_full_loader)))
+        mean_test = 0
+        for epoch in progress_bar:
+            loss = 0.0
+            losses = 0.0
+            for index, batch_features in enumerate(self.train_full_loader):
+                action = batch_features[0].squeeze(-1).permute(1,0,2).to(device)
+                tactile = batch_features[1].permute(1,0,2).to(device)
+                tactilederiv1 = batch_features[2].permute(1,0,2).to(device)
+                tactilederiv2 = batch_features[3].permute(1,0,2).to(device)
+                tactile_predictions = self.full_model.forward(tactiles=tactile, actions=action, tac_deriv1=tactilederiv1, tac_deriv2=tactilederiv2) # Step 3. Run our forward pass.
+                self.optimizer.zero_grad()
+                loss = self.criterion(tactile_predictions, tactile[context_frames:])
+                loss.backward()
+                self.optimizer.step()
+
+                losses += loss.item()
+                if index:
+                    mean = losses / index
+                else:
+                    mean = 0
+                progress_bar.set_description("epoch: {}, ".format(epoch) + "loss: {:.4f}, ".format(float(loss.item())) + "mean loss: {:.4f}, ".format(mean))
+                progress_bar.update()
+            plot_training_loss.append(mean)
+
+            val_losses = 0.0
+            val_loss = 0.0
+            with torch.no_grad():
+                for index__, batch_features in enumerate(self.valid_full_loader):
+                    action = batch_features[0].squeeze(-1).permute(1,0,2).to(device)
+                    tactile = batch_features[1].permute(1,0,2).to(device)
+                    tactilederiv1 = batch_features[2].permute(1,0,2).to(device)
+                    tactilederiv2 = batch_features[3].permute(1,0,2).to(device)
+                    tactile_predictions = self.full_model.forward(tactiles=tactile, actions=action, tac_deriv1=tactilederiv1, tac_deriv2=tactilederiv2) # Step 3. Run our forward pass.
+                    ground_truth = tactile[context_frames:]
+                    self.optimizer.zero_grad()
+                    val_loss = self.criterion(tactile_predictions[:,:,:48].to(device), ground_truth[:,:,:48])
+                    val_losses += val_loss.item()
+
+            print("Validation mean loss: {:.4f}, ".format(val_losses / index__))
+            plot_validation_loss.append(val_losses / index__)
+            if previous_val_mean_loss < val_losses / index__:
+                early_stop_clock +=1
+                previous_val_mean_loss = val_losses / index__
+                if early_stop_clock == 4:
+                    print("Early stopping")
+                    break
+            else:
+                if best_val_loss > val_losses / index__:
+                    print("saving model")
+                    torch.save(self.full_model, model_path + "model_t1_corrected")
+                    self.strongest_model = copy.deepcopy(self.full_model)
+                    best_val_loss = val_losses / index__
+                early_stop_clock = 0
+                previous_val_mean_loss = val_losses / index__
+            plt.plot(plot_training_loss, c="r", label="train loss MAE")
+            plt.plot(plot_validation_loss, c='b', label="val loss MAE")
+            plt.legend(loc="upper right")
+            plt.savefig(model_path + '/trining_plot_10steppred_new_data.png', dpi=300)
+
+
+if __name__ == "__main__":
+    BG = BatchGenerator()
+    training_dataset = BG.load_full_data()
+
+    for index, batch_features in enumerate(training_dataset):
+
+        break
